39e3e99bc33a263f93924c9444a6962890a3174b
bereilhp <bereilhp@gmail.com>
2024-07-16 15:33:54 +0200

    added collection to the fhir-api from the route parameter

--- src/fhir-api/routes/dynamic.js
+++ src/fhir-api/routes/dynamic.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { connectToDatabase } = require("../../db/mongodb-connection");
 
-router.get("/api/:resource", (req, res) => {
+function connectToMongoDB() {
+  return connectToDatabase();
+}
+
+router.get("/api/:resource", async (req, res) => {
   const resource = req.params.resource;
 
   console.log("Host URL: ", req.originalUrl);
@@ -10,16 +15,24 @@ router.get("/api/:resource", (req, res) => {
   console.log(req.params.resource);
   console.log(req.query);
 
-  if (resource === "Patients") {
-    return res
-      .status(200)
-      .json({ message: "You requested the data of Patients." });
-  } else if (resource === "Observations") {
-    return res
-      .status(200)
-      .json({ message: "You requested the data of Observations." });
-  } else {
-    return res.status(404).json({ error: "Resource not found" });
+  try {
+    const db = await connectToMongoDB();
+    const collection = db.collection(resource);
+
+    if (resource === "Patients") {
+      return res
+        .status(200)
+        .json({ message: "You requested the data of Patients." });
+    } else if (resource === "Observations") {
+      return res
+        .status(200)
+        .json({ message: "You requested the data of Observations." });
+    } else {
+      return res.status(404).json({ error: "Resource not found" });
+    }
+  } catch (error) {
+    console.error("Error in handling request:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
