b64f540aec95ddc7b48cfe737e6d4d4800d0856d
bereilhp <bereilhp@gmail.com>
2024-07-16 15:15:09 +0200

    fhir-api dynamic with db connection to mongodb

--- makefile
+++ makefile
@@ -1,11 +1,23 @@
+git: 
+	@git pull
+	@git add .
+	@git commit -m "$m" #m="your message"
+	@git push 
+
+status:
+	@git status
+
+code:
+	@code .
+
 build:
-	docker-compose up --build -d
+	docker compose up --build -d
 
 start: 
-	docker-compose start
-
+	docker compose start
+	
 stop:
-	docker-compose stop
-
+	docker compose stop
+	
 clean:
-	docker-compose down --rmi all -v
+	docker compose down --rmi all -v
---
+++ src/fhir-api/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dynamicRoutes = require("./routes/dynamic");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = 3456;
+
+app.use(bodyParser.json());
+app.use(morgan("combined"));
+
+app.use("/dynamic", dynamicRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
