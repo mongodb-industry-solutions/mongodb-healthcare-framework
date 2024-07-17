58e68ca42105050f61ac7f8648119c7b15dc732f
bereilhp <bereilhp@gmail.com>
2024-07-17 15:08:25 +0200

    change name of api route

--- src/fhir-api/app.js
+++ src/fhir-api/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const dynamicRoutes = require("./routes/dynamic");
+const dynamicRoutes = require("./routes/main");
 const morgan = require("morgan");
 
 const app = express();
@@ -9,7 +9,7 @@ const PORT = 3456;
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
-app.use("/dynamic", dynamicRoutes);
+app.use("/main", dynamicRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
