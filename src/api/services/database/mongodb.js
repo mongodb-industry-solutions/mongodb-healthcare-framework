3d87fd6d756af8b18aced2dd8ae916035c1c2cd8
bereilhp <bereilhp@gmail.com>
2024-10-15 14:43:03 +0200

    Added JSDoc to services files

--- src/api/services/database/mongodb.js
+++ src/api/services/database/mongodb.js
@@ -1,5 +1,10 @@
 const { connectToDatabase } = require("../../db/mongodb-connection");
 
+/**
+ * Establishes a connection to the MongoDB database using the provided configuration.
+ *
+ * @returns {Promise<Db>} A Promise that resolves to a Db object, which represents the connected database.
+ */
 async function connectToMongoDB() {
   return await connectToDatabase();
 }
--- src/api/services/params/paramHandler.js
+++ src/api/services/params/paramHandler.js
@@ -1,3 +1,9 @@
+/**
+ * Splits the provided query object into two objects - one containing keys starting with '_', and another containing all other keys.
+ *
+ * @param {Object} query - The input query object to split. It can contain any key-value pairs.
+ * @returns {{specialParams: Object, regularParams: Object}} An object containing the special parameters (keys that start with '_') and regular parameters (all other keys).
+ */
 function splitQueryParams(query) {
   const specialParams = {};
   const regularParams = {};
