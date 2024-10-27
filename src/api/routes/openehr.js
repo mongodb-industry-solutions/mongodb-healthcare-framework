c66cf0a692084df4c4811331c6a350da3e507abc
bereilhp <bereilhp@gmail.com>
2024-10-27 21:02:07 +0100

    Added UUID and composition api routes

--- src/api/routes/openehr.js
+++ src/api/routes/openehr.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { connectToMongoDB } = require("../services/database/mongodb");
 const { createEHRDocument } = require("../services/openehr/ehr");
+const { createComposition } = require("../services/openehr/composition");
 
 router.put("/ehr/:ehrId", async (req, res) => {
   const ehrId = req.params.ehrId;
@@ -42,6 +43,51 @@ router.get("/ehr/:ehrId", async (req, res) => {
     }
   } catch (error) {
     console.error("Error retrieving EHR data:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/ehr/:ehrId/composition", async (req, res) => {
+  const ehrId = req.params.ehrId;
+  const compositionData = req.body;
+
+  try {
+    const db = await connectToMongoDB();
+    const collection = db.collection("compositions");
+
+    const compositionDocument = createComposition(compositionData);
+
+    await collection.insertOne({
+      ehr_id: ehrId,
+      ...compositionDocument,
+    });
+
+    return res.status(201).json(compositionDocument);
+  } catch (error) {
+    console.error("Error storing composition:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/ehr/:ehrId/composition/:compositionId", async (req, res) => {
+  const { ehrId, compositionId } = req.params;
+
+  try {
+    const db = await connectToMongoDB();
+    const collection = db.collection("compositions");
+
+    const compositionDocument = await collection.findOne({
+      ehr_id: ehrId,
+      "uid.value": compositionId,
+    });
+
+    if (!compositionDocument) {
+      return res.status(404).json({ error: "Composition not found" });
+    }
+
+    return res.status(200).json(compositionDocument);
+  } catch (error) {
+    console.error("Error retrieving composition:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
