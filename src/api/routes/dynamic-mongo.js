45ddee65e4e941ab847a0eff745dc9e6b950fe05
bereilhp <bereilhp@gmail.com>
2025-02-24 15:35:16 +0100

    Read working from user-config

--- src/api/routes/dynamic-mongo.js
+++ src/api/routes/dynamic-mongo.js
@@ -5,64 +5,70 @@ const path = require("path");
 
 const router = express.Router();
 
-async function createEndpoints() {
-  const currentDir = process.cwd();
-  const filePath = path.join(currentDir, "src/data", "mongo.json");
-  const fhirData = loadJSONFromFile(filePath);
-  //console.log(JSON.stringify(fhirData, null, 2));
+async function connectDB() {
+  try {
+    return await connectToMongoDB();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+}
 
-  if (!fhirData) {
-    console.error("No data available to create endpoints.");
+async function saveAPIConfig(configFile, db) {
+  if (!configFile) {
+    console.error("No data available to save API config.");
     return;
   }
 
-  router.stack = [];
-
-  const db = await connectToMongoDB();
-
-  const fhirDataWithUsername = {
-    ...fhirData,
+  const configWithMetadata = {
+    ...configFile,
     username: "test",
     date: new Date(),
   };
 
-  const configCollection = db.collection("api_config");
-  await configCollection.insertOne(fhirDataWithUsername);
-  console.log("Inserting config file into MongoDB");
+  try {
+    const configCollection = db.collection("api_config");
+    await configCollection.insertOne(configWithMetadata);
+    console.log("API config saved to MongoDB.");
+  } catch (error) {
+    console.error("Error saving API config:", error);
+  }
+}
 
-  Object.keys(fhirData).forEach((resourceType) => {
-    console.log(resourceType);
-    const collection = db.collection(resourceType);
-    console.log(`Creating routes for ${resourceType}`);
+async function createEndpoints() {
+  try {
+    const currentDir = process.cwd();
+    const filePath = path.join(currentDir, "src/data", "user-config.json");
+    const configFile = loadJSONFromFile(filePath);
+    console.log("Loaded Config:", JSON.stringify(configFile, null, 2));
 
-    router.get(`/${resourceType}`, async (req, res) => {
-      const data = await collection.find().toArray();
-      res.json(data);
+    if (!configFile) {
+      console.error("No configuration found.");
+      return;
+    }
+
+    router.stack = [];
+    const db = await connectDB();
+
+    await saveAPIConfig(configFile, db);
+
+    Object.keys(configFile).forEach((resourceType) => {
+      const resourceConfig = configFile[resourceType];
+      if (resourceConfig.fhirOperations?.read) {
+        console.log(`Creating GET routes for ${resourceType}`);
+        const collection = db.collection(resourceType);
+
+        router.get(`/${resourceType}`, async (req, res) => {
+          const data = await collection.find().toArray();
+          res.json(data);
+        });
+      }
     });
 
-    router.get(`/${resourceType}/:id`, async (req, res) => {
-      const item = await collection.findOne({ _id: req.params.id });
-      item
-        ? res.json(item)
-        : res
-            .status(404)
-            .send(
-              `Resource of type ${resourceType} with ID ${req.params.id} not found`
-            );
-    });
-
-    router.post(`/${resourceType}`, async (req, res) => {
-      const result = await collection.insertOne(req.body);
-      res.status(201).json(result.ops[0]);
-    });
-
-    router.delete(`/${resourceType}/:id`, async (req, res) => {
-      await collection.deleteOne({ _id: req.params.id });
-      res.json({ message: "Deleted successfully" });
-    });
-  });
-
-  console.log("Dynamic endpoints created successfully.");
+    console.log("Dynamic endpoints created successfully.");
+  } catch (error) {
+    console.error("Error creating endpoints:", error);
+  }
 }
 
 router.get("/create", async (req, res) => {
